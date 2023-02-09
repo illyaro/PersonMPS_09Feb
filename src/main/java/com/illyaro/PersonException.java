1dd38721826f69cada212b039bba6e739dbe883f
Illya Rozumovskyy <75805551+illyaro@users.noreply.github.com>
2023-02-09 19:26:41 +0100

    Exception Class added

---
+++ src/main/java/com/illyaro/PersonException.java
@@ -0,0 +1,11 @@
+package com.illyaro;
+
+public class PersonException extends RuntimeException{
+    public PersonException(String message){
+        super(message);
+    }
+
+    public PersonException(){
+        super();
+    }
+}
