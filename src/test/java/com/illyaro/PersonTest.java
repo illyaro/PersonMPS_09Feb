343f8d3dff3760e7a465e13a728eba19e99fcb4e
Illya Rozumovskyy <75805551+illyaro@users.noreply.github.com>
2023-02-09 22:08:55 +0100

    Test cases defined for method averageAgePerGender

--- src/test/java/com/illyaro/PersonTest.java
+++ src/test/java/com/illyaro/PersonTest.java
@@ -1,10 +1,32 @@
 package com.illyaro;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+
+    private Person testPerson;
+
+    @BeforeEach
+    void setUp() {
+        String name = "Robin";
+        int age = 20;
+        String genderMale = "Male";
+        testPerson = new Person(name, age, genderMale);
+    }
+
+    @AfterEach
+    void tearDown() {
+        testPerson = null;
+    }
 
     @Test
     void NullNameOnCreationResultsException() {
@@ -64,4 +86,68 @@ class PersonTest {
                 "Inserted gender was: '" + otherGender + "'. Only 'Male' or 'Female' gender is accdepted");
     }
 
+    @Test
+    void NullPersonsListResultsException() {
+        List<Person> persons = null;
+        assertThrows(PersonException.class, () -> testPerson.averageAgePerGender(persons),
+                "Persons list cannot be null");
+    }
+
+    @Test
+    void EmptyPersonsListReturnArrayWithValuesMinusOneForEachGender() {
+        List<Person> persons = List.of();
+        double[] expected = {-1, -1};
+        assertArrayEquals(expected, testPerson.averageAgePerGender(persons));
+    }
+
+    @Test
+    void OnlyMalesListReturnArrayWithMinusOneInItsSecondPosition(){
+        Person secondPerson = new Person("Peter", 40, "Male");
+        Person thirdPerson = new Person("John", 60, "Male");
+        List<Person> persons = List.of(testPerson, secondPerson, thirdPerson);
+
+        double expectedMeanAge = (testPerson.age() + secondPerson.age() + thirdPerson.age())/persons.size();
+        double[] expected = {expectedMeanAge, -1};
+
+        assertArrayEquals(expected, testPerson.averageAgePerGender(persons));
+    }
+
+    @Test
+    void OnlyFemalesListReturnArrayWithMinusOneInItsSecondPosition(){
+        Person firstPerson = new Person("Anita", 15, "Female");
+        Person secondPerson = new Person("Sarah", 30, "Female");
+        Person thirdPerson = new Person("Angela", 60, "Female");
+        List<Person> persons = List.of(firstPerson, secondPerson, thirdPerson);
+
+        double expectedMeanAge = (firstPerson.age() + secondPerson.age() + thirdPerson.age())/persons.size();
+        double[] expected = {-1, expectedMeanAge};
+
+        assertArrayEquals(expected, testPerson.averageAgePerGender(persons));
+    }
+
+    @Test
+    void ListWithMalesAndFemalesReturnAnArrayWithBothNotNegativeElements(){
+        Person secondMale = new Person("Peter", 24, "Male");
+        Person thirdMale = new Person("John", 5, "Male");
+
+        Person firstFemale = new Person("Anita", 78, "Female");
+        Person secondFemale = new Person("Sarah", 36, "Female");
+        Person thirdFemale = new Person("Angela", 64, "Female");
+
+        double expectedMeanAgeMale = (testPerson.age() + secondMale.age() + thirdMale.age())/3;
+        double expectedMeanAgeFemale = (firstFemale.age() + secondFemale.age() + thirdFemale.age())/3;
+        double[] expected = {expectedMeanAgeMale, expectedMeanAgeFemale};
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(testPerson);
+        persons.add(secondMale);
+        persons.add(thirdMale);
+        persons.add(firstFemale);
+        persons.add(secondFemale);
+        persons.add(thirdFemale);
+
+        Collections.shuffle(persons);
+
+        assertArrayEquals(expected, testPerson.averageAgePerGender(persons));
+    }
 }
